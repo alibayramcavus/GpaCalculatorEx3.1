c3b170d6e91a0dc23695de356f72e757040730bc
alibayramcavus <alibayramcavus@hotmail.com>
2021-04-05 00:43:37 +0300

    initialization

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+app.get("/calculate", (req, res) => {
+    var totalCredit = 0
+    var totalScore = 0
+    console.log(req.body);
+    req.body.grades.forEach(grade => {
+        totalCredit = totalCredit + grade.credit
+        totalScore = totalScore + (grade.credit*grade.score)
+    });
+    const gpa = totalScore / totalCredit
+    console.log(gpa);
+    res.send("Your GPA: " + gpa)
+})
+
+app.get("/", (req, res) => {
+    res.send("Listening")
+})
+
+app.listen(3000, () => {
+    console.log("Server running on " + 3000)
+})
